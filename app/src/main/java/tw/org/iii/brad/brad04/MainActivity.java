2fa64b42bc21fbf5b10fe64085880d1583ecf156
iii <zeltkimo+github@gmail.com>
2020-03-08 09:15:59 +0800

    init

---
+++ app/src/main/java/tw/org/iii/brad/brad04/MainActivity.java
@@ -0,0 +1,25 @@
+package tw.org.iii.brad.brad04;
+//執行序
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void test1(View view) {
+
+    }
+    public void test2(View view) {
+
+    }
+    public void test3(View view) {
+
+    }
+}
